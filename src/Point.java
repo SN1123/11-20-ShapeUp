30e81edd9973df06299f1de60b2ddc77bf5fc1c8
Kyle Waltz <kylewaltz@yahoo.com>
2014-11-20 08:18:10 -0700

    Update Point.java

--- src/Point.java
+++ src/Point.java
@@ -1,1 +1,10 @@
 // A Point object represents a pair of (x, y) coordinates.
+public class Point {
+  int x;
+  int y;
+  
+  public Point(int initX, int initY){
+    x = initX;
+    y = initY;
+  }
+}
