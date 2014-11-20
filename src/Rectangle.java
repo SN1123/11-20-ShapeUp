63db00679f9947b0a1e85e8f02e1a8d474302c19
Kyle Waltz <kylewaltz@yahoo.com>
2014-11-20 08:15:34 -0700

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,1 +1,15 @@
 // A Rectangle object represents a rectangle defined by an origin point (bottom left corner), plus width and height.
+public class Rectangle {
+  int Origin;
+  int Width;
+  int Highth;
+  
+  public Rectangle (int initOrigin){
+    Origin = initOrigin;
+  }
+  
+  public Rectangle (int initWidth, int initHighth){
+    Width = initWidth;
+    Highth = initHighth;
+  }
+}
