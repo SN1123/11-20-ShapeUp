8e9b02ad807328bb9ce4159ede54bf9336cc25eb
Kyle Waltz <kylewaltz@yahoo.com>
2014-11-24 18:03:44 -0700

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,15 +1,78 @@
 // A Rectangle object represents a rectangle defined by an origin point (bottom left corner), plus width and height.
+
 public class Rectangle {
-  int Origin;
+  Point Origin;
   int Width;
-  int Highth;
+  int Height;
+  Point topRight;
   
-  public Rectangle (int initOrigin){
-    Origin = initOrigin;
+  public Rectangle (Point p1, Point p2)
+  {
+    topRight = p1;
+    Origin = p2;
+    Width = 0;
+    Height = 0;
   }
-  
-  public Rectangle (int initWidth, int initHighth){
-    Width = initWidth;
-    Highth = initHighth;
+  public Rectangle (Point p, int w, int h)
+  {
+    Origin = p;
+    w = Width;
+    h = Height;
+    topRight.setX(Origin.x + Width);
+    topRight.setY(Origin.y + Height);
+  }
+  public Point getBottomLeft ()
+  {
+    return Origin;
+  }
+  public Point getTopRight ()
+  {
+    return topRight;
+  }
+  public int getHeight()
+  {
+    return Height;
+  }
+  public int getWidth ()
+  {
+    return Width;
+  }
+  public double area ()
+  {
+    return Height*Width;
+  }
+  public double diagonal ()
+  {
+    return Math.sqrt(Height + Width);
+  }
+  public int parimeter ()
+  {
+    return Width + Width + Height + Height;
+  }
+  public boolean isSquare ()
+  {
+    if (Height = Width)
+    {
+      return true;
+    }
+    else 
+    {
+      return false;
+    }
+  }
+  public String toString ()
+  {
+    return "[" + Origin + ", " + topRight + "]";
+  }
+  public void translate (int x2, int y2)
+  {
+    Origin.translate(x2,y2); 
+  }
+  public void scale (int dx, int dy)
+  {
+    Width += dx;
+    Heigt += dy;
+    topRight.setX(topRight.x + dx);
+    topRight.setY(topRight.y + dy);
   }
 }
